452cbcae329a74b807eb7b6d9a6b0a6eaa50e5c1
Ashu Prasad <ashuprasad29516@gmail.com>
2023-12-21 14:12:35 -0600

    feat: added functionality to create admin user on application startup

--- src/main/java/com/springsecurity/security/SecurityApplication.java
+++ src/main/java/com/springsecurity/security/SecurityApplication.java
@@ -1,13 +1,43 @@
 package com.springsecurity.security;
 
+import com.springsecurity.security.entities.Role;
+import com.springsecurity.security.entities.User;
+import com.springsecurity.security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Fetch Admin user from database
+		User adminUser = userRepository.findByRole(Role.ADMIN);
+
+		// Check if admin user was not available in the database
+		if(adminUser == null) {
+			// Create a new user
+			User user = new User();
+
+			// Set ADMIN related details for the user
+			user.setFirstname("admin");
+			user.setSecondname("admin");
+			user.setEmail("admin@somemail.com");
+			user.setRole(Role.ADMIN);
+			user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+
+			// Persist admin data to database
+			userRepository.save(user);
+		}
+
+	}
 }
